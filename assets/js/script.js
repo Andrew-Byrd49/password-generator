f6fc17fd5f12d9d7d343fe97eafbea18dc496c69
Andrew Byrd <andrew.byrds2@gmail.com>
2022-05-07 22:58:00 -0400

    oraganized a bit and added a character pool

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var password = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -12,6 +10,21 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// Prompt Variables
+var passwordLgth;
+var passwordLgthNum = 0;
+var numberChar = true;
+var specialChar = true;
+var uppercaseChar = true;
+var lowercaseChar = true;
+
+// Character Pool
+var uppercasePool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowercasePool = "abcdefghijklmnopqrstuvwxyz".split("");
+var specialPool = "!@#$%^&*()_+".split("");
+var numberPool = "1234567890".split("");
+var finalPool;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
